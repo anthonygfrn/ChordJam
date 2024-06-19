7fcdc1b370f2b4bc8494a393ff9aaa730f5123e8
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-06-19 14:17:10 +0700

    Feat: Game Center

---
+++ ChordJam/GameCenterManager.swift
@@ -0,0 +1,8 @@
+//
+//  GameCenterManager.swift
+//  ChordJam
+//
+//  Created by Anthony on 19/06/24.
+//
+
+import Foundation
---
+++ ChordJam/Views/NavBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavBar.swift
+//  ChordJam
+//
+//  Created by Anthony on 19/06/24.
+//
+
+import SwiftUI
+
+struct NavBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavBar()
+}
