c3cd7bfe20906623bc4c42f35a7ae5349df8d2aa
Anthony Gufron <81062477+anthonygfrn@users.noreply.github.com>
2024-06-20 10:35:48 +0700

    Level 1, Onboarding, Resolve Issue

---
+++ ChordJam/Views/Level1/ModalLevel1.swift
@@ -0,0 +1,18 @@
+//
+//  ModalLevel1.swift
+//  ChordJam
+//
+//  Created by Anthony on 20/06/24.
+//
+
+import SwiftUI
+
+struct ModalLevel1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModalLevel1()
+}
